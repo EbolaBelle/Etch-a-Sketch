4cb084686e6b10d4218e37c85c6660075bbc9318
EbolaBelle <luke.zimmerman@outlook.com>
2024-08-12 17:14:48 -0400

    Edit css in createGrid, break handleMouseOver; (sanity refactor)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container');
 
+const SIZE = 700;
+
 createGrid(16);
 
 const etchNodeList = document.querySelectorAll('.gridbox');
@@ -10,12 +12,15 @@ etchNodeList.forEach(element => {
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
+        let boxSize = (SIZE / gridSize) - 1;
         const row = document.createElement('div');
-        row.classList.add("row");        
+        //row.classList.add("row");
+        row.style.cssText = `display: flex; height: ${boxSize + 'px'}; flex-direction: row;;`        
         container.appendChild(row);
         for (let j = 0; j < gridSize; j++) {
-            const gridBox = document.createElement('div');
-            gridBox.classList.add("gridbox");
+            const gridBox = document.createElement('div');            
+            gridBox.style.cssText = `flex: 0 0 ${boxSize + 'px'}; background-color: red; height: auto;`;
+            //gridBox.classList.add("gridbox");
             row.appendChild(gridBox);       
         }        
     }

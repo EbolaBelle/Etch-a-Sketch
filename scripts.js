7dff5ce5d2185962f29a03429ef8bf3752bacb5b
EbolaBelle <luke.zimmerman@outlook.com>
2024-08-12 17:34:59 -0400

    Switch up colors

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.container');
 
-const SIZE = 700;
+const SIZE = 960;
 
-createGrid(16);
+createGrid(100);
 
 const etchNodeList = document.querySelectorAll('.gridbox');
 
@@ -12,21 +12,21 @@ etchNodeList.forEach(element => {
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
-        let boxSize = (SIZE / gridSize) - 1;
+        let boxSize = (SIZE / gridSize);
         const row = document.createElement('div');
         //row.classList.add("row");
         row.style.cssText = `display: flex; height: ${boxSize + 'px'}; flex-direction: row;;`        
         container.appendChild(row);
         for (let j = 0; j < gridSize; j++) {
             const gridBox = document.createElement('div');            
-            gridBox.style.cssText = `flex: 0 0 ${boxSize + 'px'}; background-color: red; height: auto;`;
-            //gridBox.classList.add("gridbox");
+            gridBox.style.cssText = `flex: 0 0 ${boxSize + 'px'}; height: auto;`;
+            gridBox.classList.add("gridbox");
             row.appendChild(gridBox);       
         }        
     }
 }
 
 function handleMouseOver(event) {
-    event.target.classList.toggle("gridbox");
+    event.target.classList.toggle('gridbox');
     event.target.classList.toggle('etch');
 }
